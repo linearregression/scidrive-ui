b35ad259a75277f0d61ca9eac7c61bf920cebdce
Dmitry Mishin <dmitry.mishin@gmail.com>
2014-03-17 17:45:10 -0400

    Login redirect works properly

--- src/scidrive/auth/SciServerLogin.js
+++ src/scidrive/auth/SciServerLogin.js
@@ -15,6 +15,8 @@ define([
 ], function(declare, lang, fx, connect, coreFx, aspect, domConstruct, xhr, JSON, ioQuery, has, sniff, Dialog) {
     return declare("scidrive.SciServerLogin", null, {
 
+        loginPortalUrl: 'http://zinc26.pha.jhu.edu:8080/',
+
         constructor: function( /*Object*/ kwArgs) {
             lang.mixin(this, kwArgs);
             var token = ioQuery.queryToObject(dojo.doc.location.search.substr((dojo.doc.location.search[0] === "?" ? 1 : 0))).token;
@@ -28,9 +30,6 @@ define([
             if (undefined == this.credentials && !this.isShare) {
                 this.login(this);
             } else {
-                console.log("Have credential: ");
-                console.debug(this.credentials);
-                console.log("Or have share: "+this.isShare);
                 require(["scidrive/ScidrivePanel"], function(ScidrivePanel) {
                     if (undefined == dijit.byId("scidriveWidget")) {
                         var pan = new ScidrivePanel({
@@ -64,8 +63,11 @@ define([
         },
 
         login: function(component) {
-            var that = this;
-            document.location.href = "http://zinc26.pha.jhu.edu:8080/?callbackUrl=http://dimm.pha.jhu.edu/scidrive/scidrive.html";
+            var curUrl = location.protocol + '//' + location.host + location.pathname;
+            if(this.isShare)
+                curUrl += encodeURIComponent((curUrl.indexOf('?')>0)?'&':'?'+
+                    'share='+this.id);
+            document.location.href = this.loginPortalUrl+'?callbackUrl='+curUrl;
         },
 
         logout: function(vospace, component) {
@@ -84,28 +86,7 @@ define([
 
             dijit.byId("scidriveWidget")._refreshRegions();
 
-            document.location.href = "http://zinc26.pha.jhu.edu:8080/?logout=true";
-
-            // var authenticatedVospace, defaultVospace;
-
-            // for(var i in this.vospaces) {
-            //     var vospace = this.vospaces[i];
-            //     if(vospace.defaultRegion) {
-            //         defaultVospace = vospace;
-            //     }
-            //     if("undefined" === typeof authenticatedVospace && "undefined" !== typeof identity.regions[vospace.id]){
-            //         authenticatedVospace = vospace;
-            //     }
-            // }
-
-            // //First try to login to default vospace if is authenticated or don't have any authenticated at all
-            // if("undefined" !== typeof identity.regions[defaultVospace.id] || "undefined" === typeof authenticatedVospace) {
-            //     dijit.byId("scidriveWidget").loginToVO(defaultVospace, component);
-            // } else {
-            //     dijit.byId("scidriveWidget").loginToVO(authenticatedVospace, component);
-            // }
-
-                
+            document.location.href = this.loginPortalUrl+"?logout=true";
         },
 
         request: function(url, method, args) {
